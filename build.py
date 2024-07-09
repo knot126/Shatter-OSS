f28433c42dbaee568414438498a53e084b272515
knot126 <73200930+knot126@users.noreply.github.com>
2024-07-09 16:55:50 +0000

    what the fuck

--- build.py
+++ build.py
@@ -13,22 +13,55 @@ import json
 def run(cmd):
 	assert(subprocess.run(cmd).returncode == 0)
 
+YORSHEX_MESHBAKE_GIT_URL = 'https://codeberg.org/yorshex/sh-meshbake'
+EXPAT_TAR_GZ = 'https://github.com/libexpat/libexpat/releases/download/R_2_6_2/expat-win32bin-2.6.2.zip'
+ZLIB_TAR_GZ = 'https://zlib.net/zlib-1.3.1.tar.gz'
+
+# taken from CMakeLists.txt
+ZLIB_SRC_FILES = """adler32.c
+    compress.c
+    crc32.c
+    deflate.c
+    gzclose.c
+    gzlib.c
+    gzread.c
+    gzwrite.c
+    inflate.c
+    infback.c
+    inftrees.c
+    inffast.c
+    trees.c
+    uncompr.c
+    zutil.c""".split()
+
+EXPAT_SRC_FILES = "xmlparse.c  xmlrole.c  xmltok.c  xmltok_impl.c  xmltok_ns.c".split()
+
 def build_yorshex_meshbake_bundle():
 	os.chdir("build")
 	
 	# update meshbake git repo
 	if (not os.path.exists("sh-meshbake")):
-		run(['git', 'clone', 'https://codeberg.org/yorshex/sh-meshbake'])
+		run(['git', 'clone', YORSHEX_MESHBAKE_GIT_URL])
+		os.chdir("sh-meshbake")
+		run(['wget', EXPAT_TAR_GZ])
+		run(['wget', ZLIB_TAR_GZ])
+		os.makedirs("Expat", exist_ok = True)
+		run(['unzip', 'expat-win32bin-2.6.2.zip', '-d', 'Expat'])
+		run(['tar', '-xvzf', 'zlib-1.3.1.tar.gz'])
 	else:
 		os.chdir("sh-meshbake")
 		run(['git', 'pull'])
-		os.chdir("..")
 	
-	os.chdir("sh-meshbake")
 	# build for linux; we can just use shared libs that will pretty much
 	# always be available
+	print("BUILD: Linux")
 	run(['cc', '-o', 'meshbake.elf', 'meshbake.c', '-lm', '-lz', '-lexpat'])
 	# windows one here ...
+	print("BUILD: Windows")
+	os.chdir("Expat/Source")
+	run(['cmake', '.', '-D', 'CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc', '-D', 'CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++'])
+	os.chdir("../..")
+	run(['x86_64-w64-mingw32-gcc', '-o', 'meshbake.exe', '-Izlib-1.3.1', '-IExpat/Source', '-IExpat/Source/lib', 'meshbake.c'] + ["zlib-1.3.1/" + x for x in ZLIB_SRC_FILES] + ['Expat/Source/lib/' + x for x in EXPAT_SRC_FILES])
 	os.chdir("../..")
 	
 	# build the bundle file
@@ -55,7 +88,7 @@ def main():
 	os.makedirs("build", exist_ok = True)
 	
 	if (ap.install_deps):
-		run(['sudo', 'pacman', '-Syu', 'zlib', 'expat', 'mingw-w64-gcc'])
+		run(['sudo', 'pacman', '-Syu', 'zlib', 'expat', 'mingw-w64-gcc', 'unzip', 'cmake'])
 	
 	if (ap.build_yorshex_meshbake_bundle):
 		build_yorshex_meshbake_bundle()
