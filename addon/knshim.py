4ec0b9aad9ea77c7167f3d19ddff535acd497614
knot126 <73200930+knot126@users.noreply.github.com>
2024-11-11 03:37:22 +0000

    Update KnShim installer for Leaf variant

--- addon/knshim.py
+++ addon/knshim.py
@@ -7,18 +7,26 @@ from pathlib import Path
 LIB_DIR = str(Path(__file__).parent) + "/data/shim"
 
 def install(path):
+	for arch in os.listdir(f"{path}/lib"):
+		lsh_orig = f"{path}/lib/{arch}/libsmashhit.so"
+		
+		if os.path.isfile(lsh_orig):
+			os.makedirs(f"{path}/assets/native/{arch}", exist_ok=True)
+			os.replace(lsh_orig, f"{path}/assets/native/{arch}/libsmashhit.so.mp3")
+	
 	shutil.copytree(LIB_DIR, f"{path}/lib", dirs_exist_ok=True)
+	
 	amx = Path(f"{path}/AndroidManifest.xml")
 	amx.write_bytes(amx.read_bytes().replace(b'android:name="android.app.lib_name" android:value="smashhit"', b'android:name="android.app.lib_name" android:value="shim"'))
 	
 	try:
 		from . import patcher
 		
-		try:
-			patcher.patch_binary(f"{path}/lib/armeabi-v7a/libsmashhit.so", {"antitamper": []})
-			patcher.patch_binary(f"{path}/lib/arm64-v8a/libsmashhit.so", {"antitamper": []})
-		except:
-			print("Warning: Failed to patch some binaries.")
+		for arch in ["armeabi-v7a", "arm64-v8a"]:
+			try:
+				patcher.patch_binary(f"{path}/assets/native/{arch}/libsmashhit.so.mp3", {"antitamper": []})
+			except:
+				print(f"Warning: Failed to patch {arch} binary.")
 	except ImportError:
 		print("Note: The patcher could not be found, so you will need to patch libsmashhit.so against anti-tamper manually.")
 
