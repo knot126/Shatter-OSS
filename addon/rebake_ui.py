bb5623ced9946aa409942cf7fa08e7ca292c7bdd
knot126 <73200930+knot126@users.noreply.github.com>
2024-12-06 20:01:16 +0000

    Automatic menu segment detection

--- addon/rebake_ui.py
+++ addon/rebake_ui.py
@@ -53,11 +53,28 @@ class RebakeAllMeshes(Operator, butil.FolderSelectHelper):
 		default = "2",
 	)
 	
-	menu_mode: BoolProperty(
+	menu_mode: EnumProperty(
 		name = "Menu mode",
 		description = "Changes culling behaviour so that menu segments look normal",
-		default = False,
+		items = [
+			("auto", "Automatic", "Automatically detect if the segment should be baked in menu mode by the segment's path name"),
+			("false", "Disabled", "Don't use menu mode for any meshes"),
+			("true", "Enabled", "Use menu mode for all meshes"),
+		],
+		default = "auto",
 	)
+	
+	def invoke(self, a, b):
+		# For convinence, take the user to the segments folder if an assets path
+		# is detected.
+		ret = super().invoke(a, b)
+		
+		apk = butil.find_apk()
+		
+		if apk:
+			self.filepath = f"{apk}/segments/"
+		
+		return ret
 	
 	def execute(self, context):
 		context.window.cursor_set('WAIT')
@@ -71,17 +88,31 @@ class RebakeAllMeshes(Operator, butil.FolderSelectHelper):
 				if filename.endswith((".xml.mp3", ".xml.gz.mp3")):
 					count += 1
 					filepath = os.path.join(dirpath, filename)
+					menu_mode = should_use_menu_mode(self.menu_mode, filepath)
 					mesh_runner.bake(self.baker, filepath, templates, {
 						# For YMB
 						"ymb_ao": self.ambient_occlusion_quality,
-						"bake_menu_segment": self.menu_mode,
+						"bake_menu_segment": menu_mode,
 						"ymb_tiles": butil.get_setting('ymb_tiles'),
 						# For BM
 						"ABMIENT_OCCLUSION_ENABLED": self.ambient_occlusion_quality != "0",
-						"BAKE_UNSEEN_FACES": self.menu_mode,
+						"BAKE_UNSEEN_FACES": menu_mode,
 					})
 		
 		context.window.cursor_set('DEFAULT')
 		
 		self.report({'INFO'}, f"{count} meshes have been rebaked")
 		return {'FINISHED'}
+
+def should_use_menu_mode(setting, filepath):
+	if setting == "true":
+		return True
+	elif setting == "false":
+		return False
+	elif setting == "auto":
+		try:
+			return Path(filepath).parent.parts[-1] == "menu"
+		except:
+			return False
+	else:
+		return False
