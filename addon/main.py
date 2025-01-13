9fba5d3ae2875066a4cebc45568c5ab3cbc2e49f
knot126 <73200930+knot126@users.noreply.github.com>
2025-01-13 05:59:03 +0000

    Add KAS to server list

--- addon/main.py
+++ addon/main.py
@@ -232,6 +232,8 @@ def server_manager_update(_self = None, _context = None):
 			gNxToken = secrets.token_hex(24)
 			
 			gServerManager.set_params((butil.storage_path("testserver"), butil.find_apk(), gNxToken))
+		elif server_type == "knot":
+			gServerManager.set_params((butil.find_apk(),))
 		else:
 			gServerManager.set_params(tuple())
 		
@@ -1042,6 +1044,7 @@ class ShatterPreferences(AddonPreferences):
 			('none', "None", "Don't use any quick test server"),
 			('nx', "NxQuick", "The most modern server supporting Shatter Client v4 to the fullest. It is faster and more reliable, and despite supporting classic Quick Test export also supports loading entire levels. Does not support older clients"),
 			('yorshex', "Yorshex's Asset Server", "An advanced test server that allows loading an entire level from a Smash Hit assets folder for old quick test clients. It has been written by Yorshex"),
+			('knot', "Knot's Asset Server (beta)", ""),
 			('builtin', "SegServ (deprecated)", "The classic quick test server integrated with Shatter. Of the old servers, it is the simplest and fastest to use but only loads one segment at a time"),
 		],
 		update = server_manager_update,
